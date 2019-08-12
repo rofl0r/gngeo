5b66aa2e2a16f1e4b1f64f5a0a71d1512414b5c9
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-08-12 17:06:52 -0400

    Branch for building deb packages

---
+++ build-nigthly-deb-from-master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+export DEBFULLNAME="bot"
+export DEBEMAIL="bot@address.local"
+
+PROJECT=ngdevkit-gngeo
+UPSTREAM_VERSION=$(git grep AC_INIT ngdevkit:configure.ac | sed -ne 's/.*\[\(.*\)\].*/\1/p')
+read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" ngdevkit)
+DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
+
+dch -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
+git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ ngdevkit | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
+tar xf ${PROJECT}_${DEB_VERSION}.orig.tar.gz
+cd ${PROJECT}-${DEB_VERSION}
+cp -a ../debian .
+dpkg-buildpackage -rfakeroot -us -uc
