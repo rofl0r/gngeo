2e49a52cebb5bc83cc03112bed3a8098cf8fe1f4
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-11-22 19:19:44 +0100

    Support new build options and signing builds

--- build-nigthly-deb-from-master.sh
+++ build-nigthly-deb-from-master.sh
@@ -2,18 +2,29 @@
 
 set -ex
 
-export DEBFULLNAME="bot"
-export DEBEMAIL="bot@address.local"
+# Use defaults if not provided in environment
+: ${DEBFULLNAME:=bot}
+: ${DEBEMAIL:=bot@address.local}
+: ${SIGN_FLAGS:=-us -uc}
+: ${DISTRIB:=UNRELEASED}
+: ${BUILD_OPTS:=-F}
+
+export DEBFULLNAME DEBEMAIL
+
+if [ -f "${PUBKEY_FILE}" ]; then
+    chmod 700 $HOME/.gnupg
+    gpg --import ${PUBKEY_FILE}
+fi
 
 PROJECT=ngdevkit-gngeo
 UPSTREAM_VERSION=$(git grep AC_INIT origin/ngdevkit:configure.ac | sed -ne 's/.*\[\(.*\)\].*/\1/p')
 read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" origin/ngdevkit)
 DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
 
-dch -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
+dch -D ${DISTRIB} -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
 git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ origin/ngdevkit | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
 tar xf ${PROJECT}_${DEB_VERSION}.orig.tar.gz
 cd ${PROJECT}-${DEB_VERSION}
 cp -a ../debian .
 yes | mk-build-deps --install --remove
-dpkg-buildpackage -rfakeroot -us -uc
+dpkg-buildpackage -rfakeroot ${BUILD_OPTS} ${SIGN_FLAGS}
